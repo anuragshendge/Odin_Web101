c01336158d03dad348ebf54d6c5b772fcb509183
anuragshendge <shendge.anurag@gmail.com>
2015-06-27 11:26:09 -0400

    Add mouse over and ouse leave effects

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('img').mouseenter(function(){
+    $("img").fadeTo('slow',1);
+  });
+
+  $('img').mouseleave(function(){
+    $('img').fadeTo('slow',0.4);
+  });
+
+});
