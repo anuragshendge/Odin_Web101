1ee539a57e7f24abe1de6877d8198e09534c9932
anuragshendge <shendge.anurag@gmail.com>
2015-07-08 00:46:07 -0400

    WIP Update web page lay out

--- js_problems/jq_script.js
+++ js_problems/jq_script.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 	var hiddenContent = $('#intro');
 	var header =$("#header");
-		
+
 
 	hiddenContent.slideUp(0,function(){
 
 
 		header.click(function(){
 			hiddenContent.slideDown(500);
-			header.hide(300);
-
+			header.hide(300); 
 			hiddenContent.mouseleave(function(){
 				header.show(300);
 				hiddenContent.slideUp(500);	
